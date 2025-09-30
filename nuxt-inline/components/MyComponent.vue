ec8ea566d50f5b73f2b8b5331fe7a82dbc6e07d9
Mathieu <91875582+CogalTek@users.noreply.github.com>
2025-09-30 16:25:04 +0200

    fix: docker

--- nuxt-inline/components/MyComponent.vue
+++ nuxt-inline/components/MyComponent.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const props = defineProps<{ msg?: string }>()
-const count = ref(0)
+    import { ref } from 'vue'
+    const props = defineProps<{ msg?: string }>()
+    const count = ref(0)
 </script>
