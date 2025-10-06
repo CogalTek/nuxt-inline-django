31e53fab015453749f84db7eeceffee7b64b8371
Mathieu <91875582+CogalTek@users.noreply.github.com>
2025-10-06 14:29:29 +0200

    fix: prod.html rm

--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from app.views import home, prod
+from app.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home, name="home"),
-    path("prod/", prod, name="prod"),
 ]
